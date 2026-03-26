05739d4cd64805ac60b0a99ff62fc9a551385d61
Sameer Prajapati <sameerprajapati066@gmail.com>
2026-03-26 19:30:48 +0530

    Functions for favorites, recent, stats

--- src/SnippetManager.java
+++ src/SnippetManager.java
@@ -25,12 +25,15 @@ class SnippetManager {
         }
     }
 
+
     public void searchSnippet(String keyword) {
         boolean found = false;
 
         for (Snippet s : snippets) {
             if (s.getTitle().toLowerCase().contains(keyword.toLowerCase()) ||
-                s.getTags().toLowerCase().contains(keyword.toLowerCase())) {
+                s.getTags().toLowerCase().contains(keyword.toLowerCase()) ||
+                s.getDescription().toLowerCase().contains(keyword.toLowerCase()) ||
+                s.getCategory().toLowerCase().contains(keyword.toLowerCase())) {
 
                 System.out.println("\nFound:");
                 System.out.println("Title: " + s.getTitle());
@@ -47,6 +50,7 @@ class SnippetManager {
         }
     }
 
+
     public void deleteSnippet(int index) {
         if (index < 0 || index >= snippets.size()) {
             System.out.println("Invalid index.");
@@ -56,6 +60,61 @@ class SnippetManager {
         snippets.remove(index);
         System.out.println("Snippet deleted successfully.");
     }
+
+
+    public void toggleFavorite(int index) {
+        if (index < 0 || index >= snippets.size()) {
+            System.out.println("Invalid index.");
+            return;
+        }
+        snippets.get(index).toggleFavorite();
+        System.out.println("Favorite status updated.");
+    }
+
+
+    public void viewFavorites() {
+        boolean found = false;
+
+        for (Snippet s : snippets) {
+            if (s.isFavorite()) {
+                System.out.println("\n⭐ " + s.getTitle());
+                System.out.println("Code: " + s.getCode());
+                System.out.println("Category: " + s.getCategory());
+                found = true;
+            }
+        }
+
+        if (!found) {
+            System.out.println("No favorite snippets.");
+        }
+    }
+
+
+    public void viewRecent() {
+        int count = Math.min(5, snippets.size());
+
+        for (int i = snippets.size() - count; i < snippets.size(); i++) {
+            Snippet s = snippets.get(i);
+            System.out.println("\n" + s.getTitle());
+            System.out.println("Category: " + s.getCategory());
+        }
+    }
+
+
+    public void showStats() {
+        System.out.println("Total Snippets: " + snippets.size());
+
+        HashMap<String, Integer> map = new HashMap<>();
+
+        for (Snippet s : snippets) {
+            map.put(s.getCategory(), map.getOrDefault(s.getCategory(), 0) + 1);
+        }
+
+        for (String key : map.keySet()) {
+            System.out.println(key + ": " + map.get(key));
+        }
+    }
+
 
     public void saveToFile() {
         try {
